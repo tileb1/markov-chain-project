aea8f834908200777cff9a048ba5a19c35321099
Tim <tim.lebailly@gmail.com>
2019-12-01 13:03:36 +0100

    f

--- main.m
+++ main.m
@@ -18,8 +18,8 @@ nb_chain = 10000;
 P1_time = getPOverTime(chain_1(nb_chain, time, pi0), k);
 P2_time = getPOverTime(chain_2(nb_chain, time, pi0), k);
 
-max_std_of_mean1 = max(max(std(P1_time, 0, 3) / time)) % max std of average
-max_std_of_mean2 = max(max(std(P2_time, 0, 3) / time)) % max std of average
+max_std_of_mean1 = max(max(std(P1_time, 0, 3) / sqrt(time-1))) % max std of average
+max_std_of_mean2 = max(max(std(P2_time, 0, 3) / sqrt(time-1))) % max std of average
 
 % Compute mean (which is random variable of interest for which we have a
 % confidence interval) and save it
@@ -31,18 +31,24 @@ save('P_hat_chain_2.mat','P2');
 %% f) TODO
 % Plot PI over time
 time = 200;
-nb_chain = 10000;
+nb_chain = 30000;
 PI1_time = getEstimationPI(chain_1(nb_chain, time, pi0), k);
 PI4_time = getEstimationPI(chain_4(nb_chain, time, pi0), k);
 %%
 % Compute std for each state
 time_span = 50;
-cut1 = PI1_time(end-time_span:end, :);
-cut4 = PI4_time(end-time_span:end, :);
+cut1 = PI1_time(end-time_span+1:end, :);
+cut4 = PI4_time(end-time_span+1:end, :);
 
-max_std1 = max(std(cut1, 1)/length(cut1))
-max_std4 = max(std(cut2, 1)/length(cut2))
+max_std1 = max(std(cut1, 1)/ sqrt(length(cut1)));
+max_std4 = max(std(cut4, 1)/ sqrt(length(cut4)));
 
+PI_est_1 = mean(cut1, 1);
+PI_est_4 = mean(cut4, 1);
+%%
+bar(PI_est_1)
+%%
+bar(PI_est_4)
 %% g)
 
 %% h)
