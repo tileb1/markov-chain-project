04a0c93d99dc3481a3b4732ae40011c4957d9c94
Tim <tim.lebailly@gmail.com>
2019-12-01 13:33:02 +0100

    g

--- main.m
+++ main.m
@@ -45,12 +45,31 @@ max_std4 = max(std(cut4, 1)/ sqrt(length(cut4)));
 
 PI_est_1 = mean(cut1, 1);
 PI_est_4 = mean(cut4, 1);
+save('pi_hat_chain_1.mat','PI_est_1');
+save('pi_hat_chain_4.mat','PI_est_4');
 %%
 bar(PI_est_1)
 %%
 bar(PI_est_4)
 %% g)
-
+% chain 1
+diff_TV_1 = 0.5 * vecnorm(PI1_time - PI_est_1, 1, 2);
+diff_TV_1_1 = 0.5 * vecnorm(getEstimationPI(chain_1(nb_chain, time, 1), k) - PI_est_1, 1, 2);
+diff_TV_1_2 = 0.5 * vecnorm(getEstimationPI(chain_1(nb_chain, time, 2), k) - PI_est_1, 1, 2);
+diff_TV_1_3 = 0.5 * vecnorm(getEstimationPI(chain_1(nb_chain, time, 3), k) - PI_est_1, 1, 2);
+diff_TV_1_4 = 0.5 * vecnorm(getEstimationPI(chain_1(nb_chain, time, 4), k) - PI_est_1, 1, 2);
+diff_TV_1_5 = 0.5 * vecnorm(getEstimationPI(chain_1(nb_chain, time, 5), k) - PI_est_1, 1, 2);
+% chain 4
+diff_TV_4 = 0.5 * vecnorm(PI4_time - PI_est_4, 1, 2);
+diff_TV_4_1 = 0.5 * vecnorm(getEstimationPI(chain_4(nb_chain, time, 1), k) - PI_est_4, 1, 2);
+diff_TV_4_2 = 0.5 * vecnorm(getEstimationPI(chain_4(nb_chain, time, 2), k) - PI_est_4, 1, 2);
+diff_TV_4_3 = 0.5 * vecnorm(getEstimationPI(chain_4(nb_chain, time, 3), k) - PI_est_4, 1, 2);
+diff_TV_4_4 = 0.5 * vecnorm(getEstimationPI(chain_4(nb_chain, time, 4), k) - PI_est_4, 1, 2);
+diff_TV_4_5 = 0.5 * vecnorm(getEstimationPI(chain_4(nb_chain, time, 5), k) - PI_est_4, 1, 2);
+%% chain 1
+plot([diff_TV_1_1, diff_TV_1_2, diff_TV_1_3, diff_TV_1_4, diff_TV_1_5, diff_TV_1]);
+%% chain 4
+plot([diff_TV_4_1, diff_TV_4_2, diff_TV_4_3, diff_TV_4_4, diff_TV_4_5, diff_TV_4]);
 %% h)
 [~, D, W] = eig(P1);
 pi_stat = (W(:, 1) / sum(W(:, 1)))';
